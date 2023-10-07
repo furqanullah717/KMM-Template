21bb28955a1f21b7fcd978839119f84a617babca
furqan <furqanullah717@gmail.com>
2023-10-07 08:01:53 +0500

    fix android build bug

--- androidApp/src/main/java/com/codewithfk/eventhub/android/MainActivity.kt
+++ androidApp/src/main/java/com/codewithfk/eventhub/android/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.codewithfk.eventhub.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -10,8 +8,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.codewithfk.eventhub.di.AppModule
 import com.codewithfk.eventhub.event.App
+import moe.tlaster.precompose.lifecycle.PreComposeActivity
+import moe.tlaster.precompose.lifecycle.setContent
 
-class MainActivity : ComponentActivity() {
+class MainActivity : PreComposeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,18 +21,5 @@ class MainActivity : ComponentActivity() {
                 appModule = AppModule(LocalContext.current.applicationContext),
             )
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
